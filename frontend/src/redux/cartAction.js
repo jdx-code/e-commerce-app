3825b3a6935d42711b59c19071d1ce8aa55089a8
Jyotirmoy <jyotirmoydas12@gmail.com>
2024-01-08 15:53:11 +0530

    Added quantity increase/decrease/remove feature

--- frontend/src/redux/cartAction.js
+++ frontend/src/redux/cartAction.js
@@ -1,4 +1,4 @@
-import { ADD_TO_CART, REMOVE_FROM_CART, EMPTY_CART } from './constant'
+import { ADD_TO_CART, REMOVE_FROM_CART, EMPTY_CART, INCREASE_QTY, DECREASE_QTY, REMOVE_ALL_QTY } from './constant'
 
 export const addToCart = (data) => {
 
@@ -28,6 +28,39 @@ export const emptyCart = () => {
 
     return {
         type: EMPTY_CART,        
+    }       
+    
+}
+
+export const increaseQty = (productId) => {
+
+    console.warn("Increase Qty action", productId)
+    
+    return {
+        type: INCREASE_QTY,
+        payload: { productId }
+    }       
+    
+}
+
+export const decreaseQty = (productId) => {
+
+    console.warn("Decrease Qty action", productId)
+
+    return {
+        type: DECREASE_QTY,
+        payload: { productId }
+    }       
+    
+}
+
+export const removeAllQty = (productId) => {
+
+    console.warn("Remove All Qty action", productId)
+
+    return {
+        type: REMOVE_ALL_QTY,
+        payload: { productId }
     }       
     
 }
